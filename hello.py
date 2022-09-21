59239d3a0a9ea5d706e03f79f97455396d67eba0
John Weis <20269574+weisje@users.noreply.github.com>
2022-09-21 06:41:48 -0400

    added myname module

--- hello.py
+++ hello.py
@@ -4,5 +4,9 @@ hello.py is a dummy script for better familiarizing myself with the git version 
 '''
 from os import lseek
 
+import myname
+
+name = myname.getName()
 
 print("Hello Git!")
+print(f"hello {name}!")
---
+++ myname.py
@@ -0,0 +1,7 @@
+'''
+Function for returning the value of name to the calling function
+:return: str
+'''
+
+def getName():
+    return "John"
