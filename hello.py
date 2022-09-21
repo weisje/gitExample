c4371c67b4b4979b9f9bc0d989a6fdedb7b4aeb0
John Weis <20269574+weisje@users.noreply.github.com>
2022-09-21 06:33:46 -0400

    Updated comments

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 #! python3
 '''
-hello.py is a dummy script for better familiarizing myself with the git version control language/tools
+hello.py is a dummy script for better familiarizing myself with the git version control system
 '''
 from os import lseek
 
